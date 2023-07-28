d49a86e56a0737e848f5b6e8a79d63f3b7a7bab2
david0305 <kmchman@naver.com>
2023-07-28 16:45:13 +0900

    Added Tank

---
+++ Assets/02_Scripts/Game/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed = 100;
+    [SerializeField] private Rigidbody2D rigid2d;
+    // Start is called before the first frame update
+
+    public void Shoot(Vector3 direction)
+    {
+        rigid2d.AddForce(direction * speed);
+    }
+
+    private void FixedUpdate()
+    {
+        float angle = Mathf.Atan2(rigid2d.velocity.y, rigid2d.velocity.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
---
+++ Assets/02_Scripts/Game/TankObj.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class TankObj : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPref;
+    [SerializeField] private Transform arrow;
+
+    public int rorateSpeed = 10;
+    public int arrowSpeed = 20;
+    public float deg;
+
+    private int rotateDirection;
+
+
+    private void Start()
+    {
+        rotateDirection = 1;
+        deg = 30f;
+        AutoRotate().Forget();
+        AutoShoot().Forget();
+    }
+
+    async UniTask AutoRotate()
+    {
+        while (true)
+        {
+            await UniTask.Delay(10);
+            deg += Time.deltaTime * rorateSpeed * rotateDirection;
+            if (deg >= 50)
+            {
+                rotateDirection = -1;
+            }
+            else if (deg <= 20)
+            {
+                rotateDirection = 1;
+            }
+
+            float rad = deg * Mathf.Deg2Rad;
+            float cosX = Mathf.Cos(rad);
+            float signX = Mathf.Sin(rad);
+
+            arrow.localPosition = new Vector3(cosX, signX, 0);
+            arrow.eulerAngles = new Vector3(0, 0, deg);
+        }
+    }
+
+    async UniTask AutoShoot()
+    {
+        while (true)
+        {
+            await UniTask.Delay(500);
+            var bullet = GameObject.Instantiate(bulletPref).GetComponent<Bullet>();
+            bullet.transform.position = arrow.transform.position;
+            bullet.Shoot(arrow.localPosition.normalized);
+        }
+    }
+    
+    //void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.UpArrow))
+    //    {
+    //        deg += Time.deltaTime * arrowSpeed;
+    //        float rad = deg * Mathf.Deg2Rad;
+    //        float cosX = Mathf.Cos(rad);
+    //        float signX = Mathf.Sin(rad);
+    //        Debug.Log($"cosX {cosX} signX {signX} ");
+    //        arrow.localPosition = new Vector3(cosX, signX, 0);
+    //        arrow.eulerAngles = new Vector3(0, 0, deg);
+    //    }
+    //    else if (Input.GetKey(KeyCode.DownArrow))
+    //    {
+    //        deg -= Time.deltaTime * arrowSpeed;
+    //        float rad = deg * Mathf.Deg2Rad;
+    //        float cosX = Mathf.Cos(rad);
+    //        float signX = Mathf.Sin(rad);
+    //        Debug.Log($"cosX {cosX} signX {signX} ");
+    //        arrow.localPosition = new Vector3(cosX, signX, 0);
+    //        arrow.eulerAngles = new Vector3(0, 0, deg);
+    //    }
+
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        var bullet = GameObject.Instantiate(bulletPref).GetComponent<Bullet>();
+    //        bullet.transform.position = arrow.transform.position;
+    //        bullet.Shoot(arrow.localPosition.normalized);
+    //    }
+    //}
+}
