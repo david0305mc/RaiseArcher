42e76a7153ccc10b590738d09cd660d3fffbcd1e
david0305 <david0305mc@gmail.com>
2024-11-21 19:04:47 +0900

    누락 추가

--- Assets/Editor/EditorMenu.cs
+++ Assets/Editor/EditorMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Firebase.Auth;
 
 public class EditorMenu 
 {
@@ -18,4 +19,20 @@ public class EditorMenu
         DataManager.GenTableEnum();
         Debug.Log("GenerateTableCode");
     }
+    [MenuItem("Tools/SignOutFirebaseAuth")]
+    public static void RemoveFirebaseCache()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            return;
+        }
+        FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+
+        // 로그아웃 처리
+        
+        auth.CurrentUser.DeleteAsync();
+
+        Debug.Log("Authentication cache cleared.");
+    }
+
 }
